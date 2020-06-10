75de9e5ae1e554addd756a7ba559e873270bb739
Elias Eskelinen <elias.eskelinen@protonmail.com>
2020-06-10 12:59:59 +0300

    gdx

---
+++ desktop/src/games/viridi/saphire/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package games.viridi.saphire.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import games.viridi.saphire.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
